27695d625aa2c761f9c87415f413dad28fb5cee3
Aaroh <aarohmankad@gmail.com>
2015-10-25 06:38:34 -0700

    let's try async programming

--- routes/sms/saveSms.js
+++ routes/sms/saveSms.js
@@ -1,13 +1,16 @@
 var
+  $q = require('q');
   request = require('superagent'),
   TOKEN = 'xoxb-13180781681-ZxbeGfSGZJ5bBVmlFHOtlUYn';
 
 function findChannel (channelName) {
+  var deferred = $q.defer();
+
   request
     .get('https://slack.com/api/channels.list?token=' + encodeURIComponent(TOKEN) + '&pretty=1')
     .end(function (err, data) {
       if (err) {
-        console.log(err);
+        return deferred.reject(err);
       };
 
       var channels = JSON.parse(data.text).channels;
@@ -16,9 +19,12 @@ function findChannel (channelName) {
           return;
         }
 
-        return channels[i].id;
+        console.log('returning id');
+        return deferred.resolve(channels[i].id);
       };
     });
+
+  return deferred.promise;
 }
 
 module.exports = function(router) {
@@ -26,14 +32,15 @@ module.exports = function(router) {
   // create a student based on request body
   router.post('/saveSms', function (req, res) {
     var wantedChannel = req.body.Body.split(/:/)[0];
-    var channelId = findChannel(wantedChannel);
-
-    request
-      .post('https://slack.com/api/chat.postMessage')
-      .send({
-        token: TOKEN,
-        channel: channelId,
-        text: req.body.Body,
+    findChannel(wantedChannel)
+      .then(function () {
+        request
+          .post('https://slack.com/api/chat.postMessage')
+          .send({
+            token: TOKEN,
+            channel: channelId,
+            text: req.body.Body,
+          });
       });
   });
 };
