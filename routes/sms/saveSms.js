98a0fb6f1970e1508de8dedc4f061b1755df322b
Aaroh <aarohmankad@gmail.com>
2015-10-25 05:34:07 -0700

    return request body

--- routes/sms/saveSms.js
+++ routes/sms/saveSms.js
@@ -2,6 +2,6 @@ module.exports = function(router) {
   // A POST request to /api/students will 
   // create a student based on request body
   router.post('/saveSms', function (req, res) {
-    console.log(req.body);
+    res.send(req.body);
   });
 };
