065e9a857e404f51a98b922ebccf7b1122d694a5
Aaroh <aarohmankad@gmail.com>
2015-10-25 06:17:46 -0700

    send message to slack, maybe

--- routes/sms/saveSms.js
+++ routes/sms/saveSms.js
@@ -1,8 +1,37 @@
+var
+  request = require('superagent'),
+  TOKEN = 'xoxb-13180781681-ZxbeGfSGZJ5bBVmlFHOtlUYn';
+
+function findChannel (channelName) {
+  request
+    .get('https://slack.com/api/channels.list')
+    .send({
+      token: TOKEN,
+    })
+    .end(function (data) {
+      for (var i = data.channels.length - 1; i >= 0; i--) {
+        if (data.channels[i].name !== channelName) {
+          return;
+        }
+
+        return data.channels[i].id;
+      };
+    });
+}
+
 module.exports = function(router) {
   // A POST request to /api/students will 
   // create a student based on request body
   router.post('/saveSms', function (req, res) {
-    res.send(req.body);
-    console.log(req.body);
+    var wantedChannel = req.body.Body.split(/:/)[0];
+    var channelId = findChannel(wantedChannel);
+
+    request
+      .post('https://slack.com/api/chat.postMessage')
+      .send({
+        token: TOKEN,
+        channel: channelId,
+        text: req.body.Body,
+      });
   });
 };
